a4be68142b1a4b456bf289ff55090ba4a61ed7db
penkodrist <sizyu2221@gmail.com>
2024-04-12 20:44:18 +0300

    Add Node modules to GitHub repository

---
+++ index.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+require('@electron/remote/main').initialize();
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 1024,
+        height: 576,
+        minWidth: 1024,
+        minHeight: 576,
+        frame: false,
+        titleBarStyle: 'hidden',
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            devTools: true,
+            backgroundThrottling: false,
+            webviewTag: true
+        }
+    })
+    const { enable } = require('@electron/remote/main');
+    enable(win.webContents);
+    win.loadFile('index.html')
+    ipcMain.on('minimize',() =>     { win.minimize() })
+    ipcMain.on('close', () =>       { app.quit() })
+    ipcMain.on('maximize', () =>    { win.maximize() })
+    ipcMain.on('unmaximize', () =>  { win.unmaximize() })
+}
+app.whenReady().then(() => { createWindow() })
